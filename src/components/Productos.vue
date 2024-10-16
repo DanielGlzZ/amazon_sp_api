cf4752012d7c8a09ce348701440d8bf5b716544c
Jorge Reyes <rjorge99@hotmail.com>
2024-10-16 12:48:53 -0600

    se modifica la dirección de la petición marketplaceParticipaciones

--- src/components/Productos.vue
+++ src/components/Productos.vue
@@ -36,7 +36,7 @@ const authAmazon = async () => {
 
 const getMarketPlaceParticipaciones = async () => {
    try {
-      const response = await axios.get('https://api.amazon.com/sellers/v1/marketplaceParticipaciones', {
+      const response = await axios.get('https://sellingpartnerapi-na.amazon.com/sellers/v1/marketplaceParticipaciones', {
          headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
             'x-amz-access-token': sessionStorage.getItem("access_token")
